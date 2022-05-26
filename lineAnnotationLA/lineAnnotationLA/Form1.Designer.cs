bc3e43b13c36675750bd503a0d0723e41bf8f424
mehmet yiğit <56845461+mehmetygt@users.noreply.github.com>
2022-05-26 15:36:44 +0300

    Add files via upload

---
+++ lineAnnotationLA/lineAnnotationLA/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace lineAnnotationLA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPre = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rateValueY = new System.Windows.Forms.NumericUpDown();
+            this.rateValueX = new System.Windows.Forms.NumericUpDown();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rateValueY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rateValueX)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(527, 116);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 289);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedValueChanged += new System.EventHandler(this.checkedListBox1_SelectedValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(481, 370);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeChanged += new System.EventHandler(this.pictureBox1_SizeChanged);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(659, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.openFolderToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openFolderToolStripMenuItem.Text = "Open Folder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderToolStripMenuItem_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(587, 53);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(60, 27);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(527, 53);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(60, 27);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPre
+            // 
+            this.btnPre.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPre.Location = new System.Drawing.Point(527, 25);
+            this.btnPre.Name = "btnPre";
+            this.btnPre.Size = new System.Drawing.Size(60, 27);
+            this.btnPre.TabIndex = 4;
+            this.btnPre.Text = "Previous";
+            this.btnPre.UseVisualStyleBackColor = true;
+            this.btnPre.Click += new System.EventHandler(this.btnPre_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext.Location = new System.Drawing.Point(587, 25);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(60, 27);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(524, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Rate";
+            // 
+            // rateValueY
+            // 
+            this.rateValueY.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rateValueY.Location = new System.Drawing.Point(605, 90);
+            this.rateValueY.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.rateValueY.Name = "rateValueY";
+            this.rateValueY.Size = new System.Drawing.Size(42, 20);
+            this.rateValueY.TabIndex = 7;
+            this.rateValueY.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            // 
+            // rateValueX
+            // 
+            this.rateValueX.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rateValueX.Location = new System.Drawing.Point(560, 90);
+            this.rateValueX.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.rateValueX.Name = "rateValueX";
+            this.rateValueX.Size = new System.Drawing.Size(42, 20);
+            this.rateValueX.TabIndex = 7;
+            this.rateValueX.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 410);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(659, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel2.Text = "|";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 432);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.rateValueX);
+            this.Controls.Add(this.rateValueY);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPre);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Location = new System.Drawing.Point(800, 800);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "n";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rateValueY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rateValueX)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPre;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown rateValueY;
+        private System.Windows.Forms.NumericUpDown rateValueX;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ lineAnnotationLA/lineAnnotationLA/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lineAnnotationLA
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen pencil;
+        Point coordFirst;
+        Point coordCurrent;
+        bool mouseDown = false;
+        double rateX, rateY;
+        int imageSizeX, imageSizeY;
+        List<Point[]> points = new List<Point[]>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "::.:: Hattat :::.::";
+            this.Location = new Point(0,0);
+            pictureBox1.MaximumSize = new Size(
+                Convert.ToInt32(Screen.PrimaryScreen.Bounds.Size.Width - 150),
+                Convert.ToInt32(Screen.PrimaryScreen.Bounds.Size.Height - 150));
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try { 
+            // open file
+            pictureBox1.Refresh();
+            points.Clear();
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image files (*.jpg)|*.jpg";
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.ShowDialog();
+            openImage(openFileDialog1.FileName);
+            checkedListBox1.Items.Clear();
+            checkedListBox1.Items.Add(openFileDialog1.FileName);
+            if (File.Exists(openFileDialog1.FileName.Substring(0, openFileDialog1.FileName.Length-4) + ".txt"))
+            {
+                
+                checkedListBox1.SetItemChecked(0,true);
+                checkedListBox1.SelectedIndex = 0;
+            }
+            }catch (ArgumentException ee)
+            {
+                checkedListBox1.Items.Clear();
+                points.Clear();
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            coordFirst = new Point(e.X, e.Y);
+            mouseDown = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                (sender as PictureBox).Refresh();
+
+                for (int i = 0; i < points.Count; i++)
+                {
+                    drawLine(points[i][0], points[i][1]);
+                }
+                coordCurrent = new Point(e.X, e.Y);
+                drawLine( isGap(coordFirst), isGap(coordCurrent));
+            }
+        }
+        
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+            Point[] p = new Point[2];
+            p[0] = isGap(coordFirst);
+            p[1] = isGap(coordCurrent);
+
+            points.Add(p);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            pictureBox1.Refresh();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveFile(pictureBox1.ImageLocation.ToString());
+        }
+
+        private void openFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog folderDialog1 = new FolderBrowserDialog();
+                folderDialog1.ShowDialog();
+                String Folder = folderDialog1.SelectedPath;
+                foreach (String f in Directory.GetFiles(Folder))
+                {
+                    if (f.EndsWith("jpg"))
+                    {
+                        checkedListBox1.Items.Add(f);
+                    }
+                }
+                
+                foreach (String f in Directory.GetFiles(Folder))
+                {
+                    if (f.EndsWith("txt"))
+                    {
+                        //label4.Text = f.Substring(0, f.Length - 3);
+                        // label1.Text = checkedListBox1.FindString(f.Substring(0, f.Length - 3)).ToString();
+                        int index = checkedListBox1.FindString(f.Substring(0, f.Length - 3));
+                        if (index > -1)
+                        {
+                            checkedListBox1.SetItemChecked(index, true);
+                        }
+                    }
+                }
+                checkedListBox1.SelectedIndex = 0;
+            }
+            catch (ArgumentException ee)
+            {
+                /// not selected folder
+                checkedListBox1.Items.Clear();
+                points.Clear();
+            }
+        }
+
+        private void checkedListBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            //checkedListBox1.GetItemChecked(checkedListBox1.SelectedIndex);
+            pictureBox1.WaitOnLoad = true;
+            openImage(checkedListBox1.SelectedItem.ToString());
+            points.Clear();
+
+            int index = checkedListBox1.SelectedIndex;
+            if (checkedListBox1.GetItemChecked(index))
+            {
+
+                string fileName = checkedListBox1.Items[index].ToString();
+                getPoints(sender, fileName.Substring(0, fileName.Length - 3) + "txt");
+
+                for (int i = 0; i < points.Count; i++)
+                {
+
+                    Point[] p = points[i];
+                    drawLine( isGap(p[0]), isGap(p[1]));
+                }
+            }
+
+        }
+
+        private void getPoints(object sender, String label)
+        {
+            try
+            {
+                pictureBox1.Refresh();
+                List<Point> p = new List<Point>();
+                string[] lines = File.ReadAllLines(label);
+                foreach (string l in lines)
+                {
+
+                    double rrateX = (double)imageSizeX / 400, rrateY = (double)imageSizeY / 400;
+                    string[] xy = l.Split('.');
+                    Point[] p1 = new Point[2];
+                    p1[0] = isGap(p1[0]);
+                    p1[1] = isGap(p1[1]);
+                    p1[0].X = Convert.ToInt32((double)(Convert.ToDouble(xy[0]) * rrateX));
+                    p1[0].Y = Convert.ToInt32((double)(Convert.ToDouble(xy[1]) * rrateY));
+                    p1[1].X = Convert.ToInt32((double)(Convert.ToDouble(xy[2]) * rrateX));
+                    p1[1].Y = Convert.ToInt32((double)(Convert.ToDouble(xy[3]) * rrateY));
+
+                    points.Add(p1);
+                }
+            }
+            catch (FileNotFoundException except)
+            {
+                checkedListBox1.SetItemChecked(checkedListBox1.SelectedIndex,false);
+            }
+        }
+        private void drawLine(Point first, Point last)
+        {
+            pencil = new Pen(System.Drawing.Color.Red, 5);
+
+            g = pictureBox1.CreateGraphics();
+            g.DrawLine(pencil, first, last);
+
+            g.Dispose();
+        }
+
+        public void openImage(String ImgURL)
+        {
+            try
+            {
+                //toolStripStatusLabel1.Text = ImgURL.Substring(ImgURL.LastIndexOf("\\"));
+                pictureBox1.ImageLocation = ImgURL;
+                pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                System.Drawing.Image img = System.Drawing.Image.FromFile(ImgURL);
+                imageSizeX = img.Size.Width;
+                imageSizeY = img.Size.Height;
+                rateX = (double)(rateValueX.Value / img.Size.Width);
+                rateY = (double)(rateValueY.Value / img.Size.Height);
+                toolStripStatusLabel1.Text = pictureBox1.ImageLocation.ToString();
+                toolStripStatusLabel3.Text = "Height : " + pictureBox1.Size.Height + " Width : " + pictureBox1.Size.Width;
+            }
+            catch (ArgumentException except)
+            {
+                // not selected file
+            }
+            
+        }
+
+        public void saveFile(String file)
+        {
+            file = file.Substring(0, file.LastIndexOf("."));
+            file += ".txt";
+
+            if (points.Count < 1)
+            {
+                checkedListBox1.SetItemChecked(checkedListBox1.SelectedIndex, false);
+                if (File.Exists(file))
+                { 
+                    File.Delete(file);
+                }
+                return;
+            }
+            checkedListBox1.SetItemChecked(checkedListBox1.SelectedIndex,true);
+            
+            using (var writer = File.CreateText(file))
+            {
+                for (int i = 0; i < points.Count(); i += 1)
+                {
+
+                    writer.WriteLine(Convert.ToDouble(points[i][0].X * rateX) + "." 
+                        + Convert.ToDouble(points[i][0].Y * rateY) + "." 
+                        + Convert.ToDouble(points[i][1].X * rateX) + "." 
+                        + Convert.ToDouble(points[i][1].Y * rateY));
+                }
+                writer.Close();
+            }
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if(e.KeyChar == (char)Keys.Enter)
+                saveFile(pictureBox1.ImageLocation.ToString());
+            else if (e.KeyChar == (char)Keys.A || e.KeyChar == 'a' || e.KeyChar == (char)Keys.Left)
+            {
+                saveFile(pictureBox1.ImageLocation.ToString());
+                int i = checkedListBox1.SelectedIndex;
+                if (i > 0)
+                    checkedListBox1.SelectedIndex = i - 1;
+            }
+            else if (e.KeyChar == (char)Keys.D || e.KeyChar == 'd' || e.KeyChar == (char)Keys.Right)
+            {
+                saveFile(pictureBox1.ImageLocation.ToString());
+                int i = checkedListBox1.SelectedIndex;
+                if (i < checkedListBox1.Items.Count)
+                    checkedListBox1.SelectedIndex = i + 1;
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            saveFile(pictureBox1.ImageLocation.ToString());
+            int i = checkedListBox1.SelectedIndex;
+            if (i < checkedListBox1.Items.Count)
+                checkedListBox1.SelectedIndex = i + 1;
+        }
+
+        private void btnPre_Click(object sender, EventArgs e)
+        {
+            saveFile(pictureBox1.ImageLocation.ToString());
+            int i = checkedListBox1.SelectedIndex;
+            if (i > 0)
+                checkedListBox1.SelectedIndex = i - 1;
+        }
+
+        private void pictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            Rectangle screen = Screen.PrimaryScreen.WorkingArea;
+            
+            if (pictureBox1.Size.Width < screen.Width || pictureBox1.Size.Width < screen.Height)
+                Form1.ActiveForm.Size = new Size(Convert.ToInt32(pictureBox1.Size.Width * 1.30), Convert.ToInt32(pictureBox1.Size.Height * 1.30));
+            else
+                WindowState = FormWindowState.Maximized;
+            Form1.ActiveForm.Refresh();
+        }
+       
+        private Point isGap(Point size)
+        {
+            if (size.X > imageSizeX)
+                size.X = imageSizeX;
+            if (size.X > imageSizeX)
+                size.X = imageSizeX;
+            return size;
+        }
+    }
+}
